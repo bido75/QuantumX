67030d2c3b9d650c4d451c807a604b63b862704f
Vincent K. Gbewonyo <bido75@gmail.com>
2025-08-24 10:09:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def quantumx_webhook():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
